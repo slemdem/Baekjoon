3ca521407e4c8708d769e6d05e7ec88ddf46f07e
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-14 16:06:10 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+N = int(input())
+prime = []
+str = input()
+P = str.split(' ')
+
+for i in range(N):
+    p = int(P[i])
+    isprime = True
+    for j in range(2,p-1):
+        if p % j == 0:
+            isprime = False
+            break
+    if isprime == True and p != 1:
+        prime.append(p)
+
+print(len(prime))
