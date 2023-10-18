10136bff88f81c1081a1c55cbfe76a249fe5ee59
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-18 17:17:49 +0900

    [Silver II] Title: 나무 자르기, Time: 2904 ms, Memory: 143264 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def cut_tree(arr,length):
+    tall = max(arr)
+    low = 0
+    result = 0
+    
+    while low <= tall:
+        mid = (tall+low)//2
+        cutted = 0
+        
+        for i in range(N):
+            if arr[i] > mid: 
+                cutted += (arr[i] - mid)
+        
+        if cutted == length:
+            return mid
+        if cutted > length :
+            low = mid + 1
+            result = mid
+        else:
+            tall = mid - 1
+            
+    return result
+
+N, M = list(map(int,input().split()))
+trees = list(map(int,input().split()))
+
+print(cut_tree(trees,M))
