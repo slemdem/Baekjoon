8368a5787a226ec5636a9f3a0318344a3597e264
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-25 16:50:04 +0900

    [Gold V] Title: 토마토, Time: 3008 ms, Memory: 55620 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,61 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+from collections import deque
+
+
+def not_ripe(x, y, level):
+    if x < 0 or x > M*H-1 or y < 0 or y > N-1 :
+        return False
+    if x < (level*M) or x >= (level+1)*M:
+        return False
+    
+    if arr[x][y] == 0:
+        return True
+    return False
+
+def make_ripe(riped):
+    que = deque(riped)
+    cnt = 0
+    while que:
+        que_size = len(que)
+        for i in range(que_size):
+            nowx, nowy, level = que.popleft()
+            for i in range(6):
+                if not_ripe(nowx+moverange[i][0],nowy+moverange[i][1],level+moverange[i][2]):
+                    que.append([nowx+moverange[i][0],nowy+moverange[i][1],level+moverange[i][2]])
+                    arr[nowx+moverange[i][0]][nowy+moverange[i][1]] = 1
+                  
+        cnt += 1
+    return cnt -1
+            
+N, M, H = list(map(int, sys.stdin.readline().split()))
+
+allriped = True
+riped = []
+arr = [[] for _ in range(M*H)]
+for i in range(M*H):
+    arr[i] = list(map(int,input().split()))
+    for j in range(N):
+        if arr[i][j] == 1:
+            riped.append([i,j,i//M])
+        if arr[i][j] == 0:
+            allriped = False
+            
+if allriped == True:
+    print(0)
+    exit()
+    
+moverange = ((1, 0, 0),(0,1, 0),(-1,0, 0),(0,-1, 0),(M,0, 1),(-M,0, -1))
+count = make_ripe(riped)
+
+allriped = True
+for i in range(M*H):
+    for j in range(N):
+        if arr[i][j] == 0:
+            allriped = False
+
+if allriped == True:
+    print(count)
+else:
+    print(-1)
