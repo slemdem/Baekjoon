657fa4ab0df03e01e112fd0846bfa252e1a8493d
진상하 <98368934+slemdem@users.noreply.github.com>
2023-10-27 23:09:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 5700 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 N,K = input().split()
 N = int(N)
 K = int(K)
@@ -5,17 +8,17 @@ stuff = []
 Bag = [[0 for j in range(K+1)] for i in range(N+1)]
 MAXV = 0
 
+
 for i in range(N):
-    W,V = input().split()
-    W = int(W)
-    V = int(V)
+    W,V = map(int,input().split())
     stuff.append([W,V])
 
 for i in range(1,N+1):
+    occupied = 0
     for j in range(1,K+1):
         
         if stuff[i-1][0] <= j:
-            Bag[i][j] = max(Bag[i-1][j], Bag[i-1][ j-stuff[i-1][0] ] + stuff[i-1][1]  )
+            Bag[i][j] = max(Bag[i-1][j], Bag[i-1][ j-stuff[i-1][0] ] + stuff[i-1][1])
         else:
             Bag[i][j] = Bag[i-1][j]
         
