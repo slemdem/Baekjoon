88308138e552eebfe20759f07c973d294e5bae99
진상하 <98368934+slemdem@users.noreply.github.com>
2024-01-12 20:52:40 +0900

    [level Lv.2] Title: 택배 배달과 수거하기, Time: 140.49 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/Lv.2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,15 +1,12 @@
 def solution(cap, n, deliveries, pickups):
-    answer = 0
-    logGo = n
-    logcome = n
+    answer, logGo, logcome = 0, n, n
     while True:
         nowcap = cap
         destanceGo = 0
         for i in range(logGo,0,-1):
-            if(nowcap<=0):
-                break
+            if(nowcap<=0): break
             if(deliveries[i-1] == 0): continue
-            
+
             if(deliveries[i-1]<=nowcap):
                 nowcap -= deliveries[i-1]
                 deliveries[i-1] = 0
@@ -17,17 +14,16 @@ def solution(cap, n, deliveries, pickups):
             else:
                 deliveries[i-1] -= nowcap
                 nowcap = 0
-                
+
             if(destanceGo < i):
                 destanceGo = i
-                
+
         nowcap = cap
         destanceCome = 0
         for i in range(logcome,0,-1):
-            if(nowcap <= 0):
-                break
-            if(pickups[i-1]==0):continue
-            
+            if(nowcap <= 0): break
+            if(pickups[i-1]==0): continue
+
             if(pickups[i-1]<=nowcap):
                 nowcap -= pickups[i-1]
                 pickups[i-1] = 0
@@ -37,9 +33,9 @@ def solution(cap, n, deliveries, pickups):
                 nowcap = 0
             if(destanceCome < i):
                 destanceCome = i
-        
+
         answer += 2*max(destanceGo,destanceCome)
         if(destanceCome == 0 and destanceGo == 0):
             break
-        
+
     return answer
