b55f9afefddd1f277c74dd5fad65f84f2e125bf2
진상하 <98368934+slemdem@users.noreply.github.com>
2024-01-23 14:09:33 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1008 ms, Memory: 117812 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+
+A = deque(list(map(int,input().split())))
+R = deque([0]*N)
+stage = 0
+while True:
+    deque.appendleft(A,deque.pop(A))
+    deque.appendleft(R,deque.pop(R))
+    if R[N-1] == 1:
+        R[N-1] = 0
+    
+    for i in range(N-1,-1,-1):
+        if (R[i] == 1):
+            if (A[i+1] > 0 and R[i+1] != 1):
+                R[i] = 0
+                R[i+1] = 1
+                A[i+1] -= 1
+                if R[N-1] == 1:
+                    R[N-1] = 0
+    
+    if A[0] > 0 :
+        A[0] -= 1
+        R[0] = 1
+    
+    stage +=1
+    if (A.count(0) >= K):
+        break
+    
+print(stage)
