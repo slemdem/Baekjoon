7fd85edb9e6bf9e0e30d05633b9767596d3996b7
진상하 <98368934+slemdem@users.noreply.github.com>
2024-05-19 20:53:20 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,18 +1,28 @@
 def solution(progresses, speeds):
     answer = []
     dates = []
+    last = 0
     for i in range(len(progresses)):
+        onprocess = (100 - progresses[i]) // speeds[i] 
         if (100 - progresses[i]) % speeds[i] != 0:
-            dates.append((100 - progresses[i]) // speeds[i] + 1)
-        else: dates.append((100 - progresses[i]) // speeds[i])
-    deploy = 0
-    last = dates[0]
-    for i in dates:
-        if i <= last:
-            deploy += 1
-        else:
-            answer.append(deploy)
-            deploy = 1
-            last = i
-    answer.append(deploy)
+            onprocess += 1
+            dates.append(onprocess)
+        else: dates.append(onprocess)
+        
+        if last == 0: last = onprocess
+        elif onprocess > last:
+            answer.append(len(dates)-1)
+            last = onprocess
+            dates = [onprocess]
+    answer.append(len(dates))
+    # deploy = 0
+    # last = dates[0]
+    # for i in dates:
+    #     if i <= last:
+    #         deploy += 1
+    #     else:
+    #         answer.append(deploy)
+    #         deploy = 1
+    #         last = i
+    # answer.append(deploy)
     return answer
