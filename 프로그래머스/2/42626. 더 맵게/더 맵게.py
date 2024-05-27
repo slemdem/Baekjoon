aa3bf48cb0f3fe88f940ab84e488973eb08e472d
진상하 <98368934+slemdem@users.noreply.github.com>
2024-05-27 14:44:04 +0900

    [level 2] Title: 더 맵게, Time: 1642.45 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,4 +1,5 @@
 import heapq
+
 def solution(scoville, K):
     heapq.heapify(scoville)
     answer = 0
@@ -9,9 +10,9 @@ def solution(scoville, K):
             break
         min_scoville2 = heapq.heappop(scoville)
         
-        mix_scoville = min_scoville1 + (min_scoville2 * 2)
-        heapq.heappush(scoville, mix_scoville)
+        heapq.heappush(scoville, min_scoville1 + (min_scoville2 * 2))
             
         answer+=1
+        
     if scoville[0] < K: return -1
     return answer
