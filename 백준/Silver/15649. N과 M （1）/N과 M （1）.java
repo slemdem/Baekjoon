24c9ac8a5d86f5ad8c473b42f6acd0495fdf0802
진상하 <98368934+slemdem@users.noreply.github.com>
2024-08-13 15:59:19 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 22764 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N,M;
+    static boolean[] flag;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        flag = new boolean[N+1];
+        int[] arr = new int[M];
+        for (int i = 1; i<=N; i++) {
+        	flag[i]=true;
+        	arr[0]=i;
+        	backtrack(arr,1);
+        	flag[i]=false;
+        }
+        System.out.println(sb);
+    }
+    public static void backtrack(int[] arr, int j) {
+		if(j>=M) {
+			arrtoSb(arr);
+			return;
+		}
+// 	   System.out.println(i+" "+j);
+		for (int k = 1;k<=N;k++) {
+			if(flag[k]== true) continue;
+			flag[k]=true;
+			arr[j]=k;
+			backtrack(arr,j+1);
+			flag[k]=false;
+        }
+	}
+    
+    public static void arrtoSb(int[] arr) {
+		for(int i=0; i<arr.length;i++) {
+			sb.append(arr[i]);
+			if(i!=arr.length-1) sb.append(" ");
+			else sb.append("\n");
+		}
+	}
+}
