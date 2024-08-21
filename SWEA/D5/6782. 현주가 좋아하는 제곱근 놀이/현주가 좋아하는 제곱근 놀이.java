0117baa2932bcf77f1ab8126504359c428153f5a
진상하 <98368934+slemdem@users.noreply.github.com>
2024-08-21 16:17:13 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 210 ms, Memory: 32,916 KB -BaekjoonHub

--- SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -1,24 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
 	
 	public static void main(String args[]) throws Exception{
-		Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer sb = new StringBuffer();
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int test_case=1; test_case<=T; test_case++) {
-			long N = sc.nextLong();
+			long N = Long.parseLong(br.readLine());
 			int cnt=0;
 			while(N>2) {
 				if(Math.sqrt(N)%1==0.0) {
 					N=(long) Math.sqrt(N);
+					cnt++;
 				}else {
 					long until = (long) Math.pow(Math.ceil(Math.sqrt(N)),2);
-					cnt += until-N-1;
+					cnt += until-N;
 					N = until;
 				}
-				cnt++;
 			}
 			
 			sb.append("#"+test_case+" "+cnt+"\n");
