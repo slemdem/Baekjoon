740cd8b8230a7bf81f536e4880a504525db5d64c
진상하 <98368934+slemdem@users.noreply.github.com>
2024-09-04 15:26:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 109 ms, Memory: 18,736 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,54 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
 	static int day, month, month3, year;
-	static int[] plan;
-	public static void main(String[] args) throws Exception
-	{	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		
-		int T = Integer.parseInt(br.readLine());
-
-		for (int testcase = 1; testcase <= T; ++testcase)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			day = Integer.parseInt(st.nextToken());
-			month = Integer.parseInt(st.nextToken());
-			month3 = Integer.parseInt(st.nextToken());
-			year = Integer.parseInt(st.nextToken());
-
-			st = new StringTokenizer(br.readLine(), " ");
-			plan = new int[12];
-			for (int i=0; i<12; i++) {
-				plan[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			int result = Math.min(price(0),year);
-			sb.append("#"+testcase+" "+result+"\n");
+    static int[] plan;
+    public static void main(String[] args) throws Exception
+    {   
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+         
+        int T = Integer.parseInt(br.readLine());
+ 
+        for (int test = 1; test <= T; test++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            day = Integer.parseInt(st.nextToken());
+            month = Integer.parseInt(st.nextToken());
+            month3 = Integer.parseInt(st.nextToken());
+            year = Integer.parseInt(st.nextToken());
+ 
+            st = new StringTokenizer(br.readLine(), " ");
+            plan = new int[12];
+            int[] price1= new int[12];
+            for (int i=0; i<12; i++) {
+                plan[i] = Integer.parseInt(st.nextToken());
+                if(plan[i]*day>month) price1[i] = month;
+                else price1[i] =plan[i]*day;
+            }
+            
+            int[] price3 = new int[13];
+            for (int i=1; i<=12; i++) {
+            	price3[i]= price3[i-1]+price1[i-1];
+                if(i>=3) price3[i] = Math.min(price3[i-3]+month3,price3[i]);
+            }
+            int result = Math.min(price3[12],year);
+			sb.append("#"+test+" "+result+"\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static int price(int cnt) {
-		if (cnt>=12) return 0;
-		if (plan[cnt]==0) return price(cnt+1);
-		return Math.min(price1(cnt)+price(cnt+1), price3(cnt)+price(cnt+3));
-	}
-	
-	public static int price1(int cnt) {
-		if (cnt>=12) return 0;
-		return Math.min(plan[cnt]*day, month);
-	}
-	
-	public static int price3(int cnt) {
-		int sum=0;
-		for (int i=0;i<3;i++){
-			sum+=price1(cnt+i);
-            if (sum>month3) return month3;
-        }
-		return sum;
-	}
 }
