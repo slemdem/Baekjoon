114116ea5665bd9b4ab07f70020c571179850d5b
진상하 <98368934+slemdem@users.noreply.github.com>
2024-09-06 12:03:03 +0900

    [D4] Title: [Professional] 키 순서, Time: 581 ms, Memory: 93,864 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int N, M, p, c;
-    static int[][] matrix;
+	static int N, M, p, c, cnt;
+    static int[][] adjMatrix,radMatrix;
     static boolean[] visited;
     public static void main(String[] args) throws Exception
     {   
@@ -17,65 +17,61 @@ public class Solution {
             N = Integer.parseInt(br.readLine());
             M = Integer.parseInt(br.readLine());
 
-            matrix = new int[N+1][N+1];
-            
-            int[] topol = new int[N+1];
+            adjMatrix = new int[N+1][N+1];
+            radMatrix = new int[N+1][N+1];
             
             for (int i=0; i<M;i++) {
             	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             	p = Integer.parseInt(st.nextToken());
             	c = Integer.parseInt(st.nextToken());
             	
-            	matrix[p][c] = 1;
+            	adjMatrix[p][c] = 1;
+            }
+            for (int i=1; i<=N;i++) {
+            	adjMatrix[i][0]=-1;
             }
 
             int ans=0;
             for (int i=1; i<=N;i++) {
-            	if(gtBFS(i)+ltBFS(i)==N-1) ans++;
+        		if(adjMatrix[i][0]!=-1) continue;
+            	gtDFS(i);
             }
             
+
+            for (int i=1; i<=N;i++) {
+                for (int j=1; j<=N;j++) {
+                	adjMatrix[0][j] += adjMatrix[i][j];
+                }
+            }
+            
+            for (int i=1; i<=N;i++) {
+        		if(adjMatrix[i][0]+adjMatrix[0][i] ==N-1) ans++;
+            }
             
 			sb.append("#"+test+" "+ans+"\n");
 		}
 		System.out.println(sb);
 	}
-	private static int gtBFS(int start) {
-		int cnt=0;
-        Queue<Integer> q = new ArrayDeque<>();
-        visited = new boolean[N+1];
-        
-        q.offer(start);
-        visited[start] = true;
-        
-        while (!q.isEmpty()) {
-        	int cur = q.poll();
-        	for(int i=1;i<=N; i++) {
-        		if(visited[i]||matrix[cur][i]==0) continue;
-        		q.offer(i);
-        		visited[i]=true;
-        		cnt++;
-        	}
+    
+	private static void gtDFS(int cur) {
+        for(int i=1;i<=N; i++) {
+    		if(adjMatrix[cur][i]==0) continue;
+    		if(adjMatrix[i][0]==-1) {
+    			gtDFS(i);
+    		}
+
+    		if(adjMatrix[i][0]>0) {
+    			for(int j=1; j<=N; j++) {
+    				if(adjMatrix[i][j] != 0) adjMatrix[cur][j] = 1;
+    			}
+    		}
 		}
-		return cnt;
+        
+        adjMatrix[cur][0] = 0;
+        for(int k=1; k<=N; k++) {
+			adjMatrix[cur][0] += adjMatrix[cur][k];
+		}
 		
 	}
-	private static int ltBFS(int start) {
-		int cnt=0;
-        Queue<Integer> q = new ArrayDeque<>();
-        visited = new boolean[N+1];
-        
-        q.offer(start);
-        visited[start] = true;
-        
-        while (!q.isEmpty()) {
-        	int cur = q.poll();
-        	for(int i=1;i<=N; i++) {
-        		if(visited[i]||matrix[i][cur]==0) continue;
-        		q.offer(i);
-        		visited[i]=true;
-        		cnt++;
-        	}
-		}
-		return cnt;
-	}
+	
 }
