09e8cca86cdceaa7b57e6c6f279989b9ef66c657
진상하 <98368934+slemdem@users.noreply.github.com>
2024-09-23 16:53:39 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1788 ms, Memory: 130556 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int[] arr = new int[N];
+		for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		int s, e, M = Integer.parseInt(br.readLine());
+		boolean pd;
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			s = Integer.parseInt(st.nextToken())-1;
+			e = Integer.parseInt(st.nextToken())-1;
+			pd = true;
+			while (s<=e) {
+				if(arr[s]!=arr[e]) pd=false;
+				s++;
+				e--;
+			}
+			
+			if(pd) sb.append("1\n");
+			else sb.append("0\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
